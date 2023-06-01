210fcb5bc69793196614caf3b83e0f53764a47bc
André Augusto <alcaugusto1985@gmail.com>
2023-06-01 12:08:05 -0300

    Variáveis em Js

---
+++ variaveis/ex001.js
@@ -0,0 +1,13 @@
+/* 1. Crie uma variável para por o nome do carro, atrubua o valor Ferrari e exiba  no console */
+let carro = "Ferrari"
+console.log(carro)
+
+/* 2. Crie uma variável para armazenar o preço do bolo que custa 19,99 e exiba no console */
+let precoBolo = 19.99
+console.log(precoBolo)
+
+/* 3. Corrija o código abaixo e exiba no console:
+   let "cidade" =  São Paulo */
+   let cidade = "São Paulo"
+   console.log(cidade)
+   
---
+++ variaveis/tipos-de-variaveis.js
@@ -0,0 +1,19 @@
+/* Tipos de variáveis em javascript */
+let nome = "André"
+console.log(typeof nome)
+console.log("==================================")
+
+let idade = 38
+console.log(typeof idade)
+console.log("==================================")
+
+let adulto = true
+console.log(typeof adulto)
+console.log("==================================")
+
+let numeros = [1, 2, 3, 4, 5, 6, 7]
+console.log(typeof numeros)
+console.log("==================================")
+
+let obj = { nome: "André", sobrenome: "Augusto"}
+console.log(typeof obj)
